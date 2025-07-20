6b5f5a40a9d00c225abf6b496fd4f3671a7cb2ad
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-07-20 15:37:32 +0500

    fix: Update service titles for clarity and consistency

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -3,25 +3,25 @@ import React from 'react';
 function Services() {
   const services = [
   {
-    title: "Electrician",
+    title: "Electrical Service",
     description:
       "Our certified electricians provide expert electrical services including safe wiring, modern lighting installations, appliance fittings, fault diagnosis, and regular maintenance for homes, offices, and commercial setups. Ensuring quality, safety, and reliability is our top priority.",
     image: "https://plus.unsplash.com/premium_photo-1661911309991-cc81afcce97d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZWxlY3RyaWNpYW58ZW58MHx8MHx8fDA%3D",
   },
   {
-    title: "Plumber",
+    title: "Plumbing Service",
     description:
       "We offer professional plumbing services including leak detection and repair, pipe installations, drainage system setup, bathroom and kitchen fittings, and complete water system maintenance. Our team ensures prompt service, high-quality materials, and long-lasting solutions for residential and commercial properties.",
     image: "https://plus.unsplash.com/premium_photo-1664298589198-b15ff5382648?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGx1bWJlcnxlbnwwfHwwfHx8MA%3D%3D",
   },
   {
-    title: "Carpenter",
+    title: "Carpentry",
     description:
       "Our skilled carpenters specialize in custom furniture making, woodwork repairs, structural carpentry, door and window installations, and decorative wood finishes. From modern to traditional designs, we deliver craftsmanship that enhances both functionality and aesthetics for your living or work space.",
     image: "https://images.unsplash.com/photo-1595844730289-b248c919d6f9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y2FycGVudGVyfGVufDB8fDB8fHww",
   },
   {
-    title: "AC Repair",
+    title: "AC Maintenance",
     description:
       "Stay cool year-round with our reliable AC repair services. We handle air conditioner installations, gas refilling, deep cleaning, filter replacements, and troubleshooting for all major brands. Count on us for efficient, affordable cooling system care with quick response times.",
     image: "https://plus.unsplash.com/premium_photo-1682126009570-3fe2399162f7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YWMlMjByZXBhaXJ8ZW58MHx8MHx8fDA%3D",
